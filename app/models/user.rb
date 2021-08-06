dd95e9e985ab9bbc127f49c68431e67e7c630d17
MotokiTakadhima <pageboy.jr@icloud.com>
2021-08-06 23:31:23 +0900

    ナビバーにログインボタンを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,6 @@ class User < ApplicationRecord
   def self.guest
     find_or_create_by!(email: "guest@example.com") do |user|
       user.password = SecureRandom.urlsafe_base64
-      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
-      # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
     end
   end
 end
