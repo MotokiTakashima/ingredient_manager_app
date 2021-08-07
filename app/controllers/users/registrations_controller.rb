1c30bb44617aadaee230e813e91f3fff868ea749
MotokiTakadhima <pageboy.jr@icloud.com>
2021-08-07 10:30:09 +0900

    ゲストユーザーのメールアドレス・パスワードを変更できないよう実装

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,9 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :ensure_normal_user, only: :destroy
+  before_action :ensure_normal_user, only: %i[update destroy]
 
   def ensure_normal_user
     if resource.email == "guest@example.com"
-      redirect_to root_path, alert: "ゲストユーザーは削除できません。"
+      redirect_to root_path, alert: "ゲストユーザーの更新・削除はできません。"
     end
   end
 end
