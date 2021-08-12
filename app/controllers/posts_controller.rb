a715842b5701ba07e0326f81775ef6064c257e2b
MotokiTakadhima <pageboy.jr@icloud.com>
2021-08-12 11:21:38 +0900

    コントローラーにstart_timeを追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -47,6 +47,6 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:product, :date)
+    params.require(:post).permit(:product, :date, :start_time)
   end
 end
