32743b425cfb98bad5631cc7820dabc8c604d882
MotokiTakadhima <pageboy.jr@icloud.com>
2021-08-22 23:57:57 +0900

    showを削除

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,10 @@
 class PostsController < ApplicationController
 
-  before_action :set_post, only: %i[show edit update destroy]
+  before_action :set_post, only: %i[edit update destroy]
 
   def index
     @posts = Post.order(id: :asc)
     @post = Post.new
-  end
-
-  def show
   end
 
   def new
