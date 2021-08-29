b3aba78b53969d9ee6873bba6e54be6caf95049a
MotokiTakadhima <pageboy.jr@icloud.com>
2021-08-29 22:49:38 +0900

    マイグレーションファイルを変更

--- db/migrate/20210727124143_create_posts.rb
+++ db/migrate/20210727124143_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
       t.string :product
-      t.date :purchase
+      t.string :memo
       t.date :start_time
 
       t.timestamps
