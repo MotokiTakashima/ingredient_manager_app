d583059c8fd52d1998c9c59a39b475bc07737aba
MotokiTakadhima <pageboy.jr@icloud.com>
2021-09-04 18:04:22 +0900

    ルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
        passwords: "users/passwords"
   }
   resources :posts
+  resources :memos
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
   end
