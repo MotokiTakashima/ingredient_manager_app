03e67818fccd95e1488b434b064437470c60d7e8
MotokiTakadhima <pageboy.jr@icloud.com>
2021-09-13 20:21:22 +0900

    enum を設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ApplicationRecord
   validates :product, presence: true, length: { maximum: 30 }
   validates :genre, presence: true
+  enum genre: {
+    others: 0,
+    vegetable: 1,
+    meat: 2,
+  }
 end
