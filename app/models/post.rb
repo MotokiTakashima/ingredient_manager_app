f360ac1a9737aae825d1b8e9bf29292d187c7f38
MotokiTakadhima <pageboy.jr@icloud.com>
2021-09-25 20:32:45 +0900

    日付のバリデーションを実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   validates :product, presence: true, length: { maximum: 30 }
   validates :genre, presence: true
+  validates :start_time, presence: true
   enum genre: {
     others: 0,
     vegetable: 1,
