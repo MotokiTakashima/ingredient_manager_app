a282220c898b4da686fd7f4ee7cf8282e214ddcd
MotokiTakadhima <pageboy.jr@icloud.com>
2021-10-21 20:25:22 +0900

    youtube検索ページを実装

--- app/controllers/youtube_controller.rb
+++ app/controllers/youtube_controller.rb
@@ -1,4 +1,25 @@
 class YoutubeController < ApplicationController
+  GOOGLE_API_KEY = ENV["API_KEY"]
+
+  def find_videos(keyword, after: 1.months.ago, before: Time.now)
+    service = Google::Apis::YoutubeV3::YouTubeService.new
+    service.key = GOOGLE_API_KEY
+
+    keyword = params[:search]
+    next_page_token = nil
+    opt = {
+      q: keyword,
+      type: "video",
+      max_results: 6,
+      order: :date,
+      page_token: next_page_token,
+      published_after: after.iso8601,
+      published_before: before.iso8601,
+    }
+    service.list_searches(:snippet, opt)
+  end
+
   def index
+    @youtube_data = find_videos(@keyword)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get "search" => "youtube#search"
   resources :posts
   resources :memos
+  resources :youtube
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
   end
