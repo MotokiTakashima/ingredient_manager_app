4a88a2997b6b701e22b7ad40ce3d7e196f7daff6
MotokiTakadhima <pageboy.jr@icloud.com>
2021-11-04 21:24:49 +0900

    タグレコードを削除

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,9 +5,4 @@ class Post < ApplicationRecord
 
   validates :product, presence: true, length: { maximum: 30 }
   validates :start_time, presence: true
-  enum genre: {
-    others: 0,
-    vegetable: 1,
-    meat: 2,
-  }
 end
