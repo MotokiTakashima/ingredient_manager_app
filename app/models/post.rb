44d9f64a9e42b9d094ecff37537d19c9d660ab7b
MotokiTakadhima <pageboy.jr@icloud.com>
2021-11-04 21:23:32 +0900

    postsコントローラーにmerge(user_id: current_user.id)を追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,9 @@ class Post < ApplicationRecord
 
   validates :product, presence: true, length: { maximum: 30 }
   validates :start_time, presence: true
+  enum genre: {
+    others: 0,
+    vegetable: 1,
+    meat: 2,
+  }
 end
