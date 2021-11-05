648bd26df78bc3c2f5d17849bbd8cab007ef1c46
MotokiTakadhima <pageboy.jr@icloud.com>
2021-11-05 23:38:45 +0900

    PostGenreにuser_idを関連づけ

--- app/models/post_genre.rb
+++ app/models/post_genre.rb
@@ -1,4 +1,5 @@
 class PostGenre < ApplicationRecord
+  belongs_to :user, optional: true
   belongs_to :post
   belongs_to :genre
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_04_124859) do
+ActiveRecord::Schema.define(version: 2021_11_04_134816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2021_11_04_124859) do
     t.bigint "genre_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
     t.index ["genre_id"], name: "index_post_genres_on_genre_id"
     t.index ["post_id"], name: "index_post_genres_on_post_id"
   end
