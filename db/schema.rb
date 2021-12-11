af188aace5b94c07859c71f80c00772fe18b8141
MotokiTakadhima <pageboy.jr@icloud.com>
2021-12-11 17:29:43 +0900

    訂正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_11_070331) do
+ActiveRecord::Schema.define(version: 2021_10_31_081520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
